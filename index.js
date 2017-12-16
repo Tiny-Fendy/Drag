50b776fa2ad3f0de1f87d9ffb7331033c78ceac5
Fendy <281966728@qq.com>
2017-12-16 18:33:47 +0800

    展开折叠完成
    Signed-off-by: Fendy <281966728@qq.com>

--- index.js
+++ index.js
@@ -21,14 +21,6 @@ let utils = {
 		dom.className = dom.className.replace(reg, '');
 	},
 
-	toggle(dom, className) {
-		if (dom.className.match(className)) {
-			this.removeClass(dom, className);
-		} else {
-			this.addClass(dom, className);
-		}
-	},
-
 	appendByIndex(parents, node, index) {
 		if (parents.children.length === index + 1) {
 			parents.appendChild(node);
@@ -45,11 +37,16 @@ class Drag {
 		this.options = options || {};
 		this.overNode = null; //当前经过的节点
 		this.curNode = null; //当前被选中的节点
+		this.activeNode = [null, null]; //当前的展开节点
 
 		this.init = this.init.bind(this);
 		this.setDragDrop = this.setDragDrop.bind(this);
 		this.setParent = this.setParent.bind(this);
 		this.setChild = this.setChild.bind(this);
+		this.toggleParents = this.toggleParents.bind(this);
+		this.toggleChild = this.toggleChild.bind(this);
+		this.closeNode = this.closeNode.bind(this);
+		this.openNode = this.openNode.bind(this);
 		this.init();
 	}
 
@@ -85,6 +82,16 @@ class Drag {
 
 	setParent(dom) {
 		dom.className = this.options.parent;
+
+		// 绑定展开折叠按钮
+		$(dom.children).forEach(icon => {
+			if (icon.nodeName === 'I') {
+				icon.onclick = () => {
+					this.toggleParents(dom);
+				}
+			}
+		});
+
 		dom.ondragstart = ev => {
 			ev.stopPropagation();
 			ev.target.style.backgroundColor = '#89c5f1';
@@ -128,6 +135,11 @@ class Drag {
 	setChild(dom) {
 		dom.className = this.options.child;
 
+		// 绑定展开折叠按钮
+		dom.querySelector('i').onclick = () => {
+			this.toggleChild(dom);
+		};
+
 		dom.ondragstart = ev => {
 			ev.stopPropagation();
 			ev.target.style.backgroundColor = '#89c5f1';
@@ -159,6 +171,60 @@ class Drag {
 		}
 
 		dom.ondragover = undefined;
+	}
+
+	toggleParents(curDom) {
+		let parent = this.activeNode[0];
+		let child = this.activeNode[1];
+		let length = curDom.querySelectorAll('.child').length;
+
+		if (parent) {
+			this.closeNode(parent, 0);
+			if (!curDom.isSameNode(parent)) {
+				this.openNode(curDom, 0, length * 35);
+				if (child) this.closeNode(child, 1);
+			}
+		} else {
+			this.openNode(curDom, 0, length * 35);
+		}
+	}
+
+	toggleChild(curDom) {
+		let parent = this.activeNode[0].querySelector('.children');
+		let child = this.activeNode[1];
+
+		if (child) {
+			this.closeNode(child, 1);
+
+			// 自身折叠则清理activeNode，同时还原容器高度
+			if (curDom.isSameNode(child)) {
+				parent.style.height = `${parent.clientHeight - 40}px`;
+			} else {
+				this.openNode(curDom, 1, 40);
+			}
+		} else {
+			parent.style.height = `${parent.clientHeight + 40}px`;
+			this.openNode(curDom, 1, 40);
+		}
+	}
+
+	// 关闭节点
+	closeNode(dom, type) {
+		let wrap = dom.querySelector('.children');
+
+		utils.removeClass(wrap, 'active');
+		dom.querySelector('i').className = 'fa fa-plus';
+		wrap.style.height = 0;
+		this.activeNode[type] = null;
+	}
+
+	openNode(dom, type,height) {
+		let wrap = dom.querySelector('.children');
+
+		wrap.style.height = height + 'px';
+		dom.querySelector('i').className = 'fa fa-minus';
+		utils.addClass(wrap, 'active');
+		this.activeNode[type] = dom;
 	}
 }
 
